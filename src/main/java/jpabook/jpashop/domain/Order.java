f5835bf379d81a994ed6eebaa892a8bedb7c4602
RyuJungSik <xvbc123@gmail.com>
2023-05-28 15:03:31 +0900

    Feat : Lesson25

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,15 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="ORDERS")
+@Table(name = "ORDERS")
 public class Order {
-@Id @GeneratedValue
+@Id
+@GeneratedValue
 @Column(name = "ORDER_ID")
 private Long id;
 
 @ManyToOne
-@JoinColumn(name="MEMBER_ID")
+@JoinColumn(name = "MEMBER_ID")
 private Member member;
+
+@OneToOne
+@JoinColumn(name = "DELIVERY_ID")
+private Delivery delivery;
+
 @OneToMany(mappedBy = "order")
 private List<OrderItem> orderItems = new ArrayList<>();
 
