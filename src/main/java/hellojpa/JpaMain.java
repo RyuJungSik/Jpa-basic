259a1b930735c20d87d6f664743f9c0bdc36695c
RyuJungSik <xvbc123@gmail.com>
2023-05-28 19:30:09 +0900

    Feat : Lesson26 - Mapped Superclass

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -14,19 +15,15 @@ public static void main(String[] args) {
     
     try {
     
-        Movie movie = new Movie();
-        movie.setDirector("aaaa");
-        movie.setActor("bbb");
-        movie.setName("naaa");
-        movie.setPrice(10000);
+        Member2 member = new Member2();
+        member.setUsername("user1");
+        member.setCreatedBy("kim");
+        member.setCreatedDate(LocalDateTime.now());
         
-        em.persist(movie);
+        em.persist(member);
         
         em.flush();
         em.clear();
-    
-        Movie findMovie = em.find(Movie.class, movie.getId());
-        System.out.println("findMovie = " + findMovie);
         
         tx.commit();
     } catch (Exception e) {
